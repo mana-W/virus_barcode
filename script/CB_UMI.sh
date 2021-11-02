e77ab1c2c97d46d2bc3ecc76f88d3e2c9b0b4e13
mana-W <83333092+mana-W@users.noreply.github.com>
2021-11-02 22:48:11 +0800

    Update CB_UMI.sh

--- script/CB_UMI.sh
+++ script/CB_UMI.sh
@@ -15,8 +15,8 @@ umi_tools extract --bc-pattern=CCCCCCCCCCCCCCCCNNNNNNNNNNNN \
                   --whitelist=${path}/whitelist.txt
                   
 mkdir UMI_CB_umitools
-zcat ${path}/R2_extracted.fastq.gz | sed -n '1~4p' | awk -F "_" '{print $1,$2,$3}' | awk '{print$1,$2,$3}' > ${path}/UMI_CB_umitools/ID_CB_UMI.tsv
-zcat ${path}/R2_extracted.fastq.gz | sed -n '2~4p' > ${path}/UMI_CB_umitools/Reads.tsv
-paste ${path}/UMI_CB_umitools/ID_CB_UMI.tsv ${path}/UMI_CB_umitools/Reads.tsv | awk -v OFS="\t" '{print$1,$4,$2,$3}' > ${path}/UMI_CB_umitools/cb.umi.tsv
-awk '$3!="" && $4!=""' ${path}/UMI_CB_umitools/cb.umi.tsv > ${path}/UMI_CB_umitools/CB_UMI.tsv
-sed -i '1i\Read.ID\tRead.Seq\tCell.BC\tUMI' ${path}/UMI_CB_umitools/CB_UMI.tsv
+zcat ${path}/R2_extracted.fastq.gz | sed -n '1~4p' | awk -F "_" '{print $1,$2,$3}' | awk '{print$1,$2,$3}' > UMI_CB_umitools/ID_CB_UMI.tsv
+zcat ${path}/R2_extracted.fastq.gz | sed -n '2~4p' > UMI_CB_umitools/Reads.tsv
+paste UMI_CB_umitools/ID_CB_UMI.tsv UMI_CB_umitools/Reads.tsv | awk -v OFS="\t" '{print$1,$4,$2,$3}' > UMI_CB_umitools/cb.umi.tsv
+awk '$3!="" && $4!=""' UMI_CB_umitools/cb.umi.tsv > UMI_CB_umitools/CB_UMI.tsv
+sed -i '1i\Read.ID\tRead.Seq\tCell.BC\tUMI' UMI_CB_umitools/CB_UMI.tsv
